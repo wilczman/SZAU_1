61497100a87925e4ed7114dce4a89604c359652a
wilczman <38138814+wilczman@users.noreply.github.com>
2020-11-07 17:43:35 +0100

    poprawka
    działa

--- dv1dt.m
+++ dv1dt.m
@@ -1,3 +1,18 @@
 function [outputArg1] = dv1dt(V1,V2,F1,Fd,alfa1,alfa2,C1,C2)
-outputArg1 = F1 + Fd - alfa1*(V1/C1)^(1/4);
+    if F1 + Fd < 0
+        a = 0;
+    else
+        a =  F1 + Fd;
+    end
+    if alfa1*(V1/C1)^(1/4) < 0
+        b = 0;
+    else
+        b = alfa1*(V1/C1)^(1/4);
+    end
+    if a - b <0
+        outputArg1 = 0;
+    else
+        outputArg1 = a -b;
+    end
+% outputArg1 = F1 + Fd - alfa1*(V1/C1)^(1/4);
 end
--- dv2dt.m
+++ dv2dt.m
@@ -1,3 +1,18 @@
 function [outputArg1] = dv2dt(V1,V2,F1,Fd,alfa1,alfa2,C1,C2)
-outputArg1 = alfa1*(V1/C1)^(1/4) - alfa2*(V2/C2)^(1/6);
+    if alfa1*(V1/C1)^(1/4) < 0
+        a = 0;
+    else
+        a = alfa1*(V1/C1)^(1/4);
+    end
+    if alfa2*(V2/C2)^(1/6) < 0
+        b = 0;
+    else
+        b = alfa2*(V2/C2)^(1/6);
+    end
+    if a - b <0
+        outputArg1 = 0;
+    else
+        outputArg1 = a -b;
+    end
+% outputArg1 = alfa1*(V1/C1)^(1/4) - alfa2*(V2/C2)^(1/6);
 end
